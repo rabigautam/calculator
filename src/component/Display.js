7c85967ef466624d0713e27b5102639c1ae1595e
Rabi Gautam <rabi@sharelookapp.com>
2022-11-24 19:40:27 +0545

    fix:add display css

--- src/component/Display.js
+++ src/component/Display.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import "./Display.css";
 export default class Display extends React.Component {
   static propTypes = {
     value: PropTypes.string
