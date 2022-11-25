34df3287b897b592ee245f77885d6f96b3054dcc
Rabi Gautam <rabi@sharelookapp.com>
2022-11-25 07:28:16 +0545

    feat:add ButtonPanel Button and logic behind

--- src/component/App.js
+++ src/component/App.js
@@ -1,27 +1,24 @@
 import React from "react";
 import Display from "./Display";
-import "./App.css"
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
 export default class App extends React.Component {
-    state = {
-        next:null,
-        total:null,
-        operation:null,
-
-    };
-    handleClick =(buttonName)=>{
-        this.setState(calculate(this.state,buttonName));
-    }
-    render(){
-       
-        return (
-            <div className="component-app">
-                <Display value={this.state.next || this.state.total || "0"} />
-            </div>
-        )
-    }
-}
-
-const calculate= ()=>{
-    return;
+  state = {
+    next: null,
+    total: null,
+    operation: null
+  };
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
